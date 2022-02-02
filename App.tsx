c2769a466d7038c602bd9e7271b402b666d101f9
nafees nazik <nafeesnazik10@gmail.com>
2022-02-03 00:59:43 +0530

    add inter font

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import useCachedResources from "./src/hooks/useCachedResources";
 import Navigation from "./src/navigation";
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <Navigation />
-      <StatusBar />
-    </SafeAreaProvider>
-  );
+  const isLoadingComplete = useCachedResources();
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
 }
---
+++ src/hooks/useCachedResources.ts
@@ -0,0 +1,33 @@
+import { Feather } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+
+export default function useCachedResources() {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  // Load any resources or data that we need prior to rendering the app
+  useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHideAsync();
+
+        // Load fonts
+        await Font.loadAsync({
+          ...Feather.font,
+          "Inter-Black": require("../../assets/fonts/Inter-Black.otf"),
+        });
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAndDataAsync();
+  }, []);
+
+  return isLoadingComplete;
+}
