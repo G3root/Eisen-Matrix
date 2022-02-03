5a0d444cb59da892132317ea1dda3254f48e08f1
nafees nazik <nafeesnazik10@gmail.com>
2022-02-03 15:00:43 +0530

    add inter as default font

--- App.tsx
+++ App.tsx
@@ -3,8 +3,79 @@ import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import useCachedResources from "./src/hooks/useCachedResources";
 import Navigation from "./src/navigation";
-import { Provider as PaperProvider } from "react-native-paper";
+import {
+  Provider as PaperProvider,
+  DefaultTheme,
+  configureFonts,
+} from "react-native-paper";
 import { Feather } from "@expo/vector-icons";
+
+const fontConfig = {
+  web: {
+    regular: {
+      fontFamily: "Inter-Black",
+      fontWeight: "400" as "400",
+    },
+    medium: {
+      fontFamily: "Inter-Black",
+      fontWeight: "500" as "500",
+    },
+    light: {
+      fontFamily: "Inter-Black",
+      fontWeight: "300" as "300",
+    },
+    thin: {
+      fontFamily: "Inter-Black",
+      fontWeight: "100" as "100",
+    },
+  },
+  ios: {
+    regular: {
+      fontFamily: "Inter-Black",
+      fontWeight: "400" as "400",
+    },
+    medium: {
+      fontFamily: "Inter-Black",
+      fontWeight: "500" as "500",
+    },
+    light: {
+      fontFamily: "Inter-Black",
+      fontWeight: "300" as "300",
+    },
+    thin: {
+      fontFamily: "Inter-Black",
+      fontWeight: "100" as "100",
+    },
+  },
+  default: {
+    regular: {
+      fontFamily: "Inter-Black",
+      fontWeight: "normal" as "normal",
+    },
+    medium: {
+      fontFamily: "Inter-Black",
+      fontWeight: "normal" as "normal",
+    },
+    light: {
+      fontFamily: "Inter-Black",
+      fontWeight: "normal" as "normal",
+    },
+    thin: {
+      fontFamily: "Inter-Black",
+      fontWeight: "normal" as "normal",
+    },
+  },
+};
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#3498db",
+    accent: "#212121",
+  },
+  fonts: configureFonts(fontConfig),
+};
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -16,6 +87,7 @@ export default function App() {
         settings={{
           icon: (props) => <Feather {...props} />,
         }}
+        theme={theme}
       >
         <SafeAreaProvider>
           <Navigation />
