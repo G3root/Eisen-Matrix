39660ec826f6dd7e303e61045b7768127613ca71
nafees nazik <nafeesnazik10@gmail.com>
2022-02-03 22:04:27 +0530

    add input component

---
+++ src/components/InputPrimitives/Input/Input.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { HelperText, TextInput } from "react-native-paper";
+
+import { Controller, UseControllerProps, Control, Path } from "react-hook-form";
+
+type Form = Omit<UseControllerProps, "control" | "name">;
+
+export interface IInputProps<Field> {
+  inputProps: React.ComponentProps<typeof TextInput>;
+  control: Control<Field>;
+  name: Path<Field>;
+  errors?: string;
+}
+
+export function Input<Field>({
+  name,
+  control,
+  rules,
+  inputProps,
+  errors,
+}: IInputProps<Field> & Form) {
+  return (
+    <>
+      <Controller
+        control={control}
+        rules={rules}
+        name={name}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            value={value}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            {...inputProps}
+            error={errors ? true : false}
+          />
+        )}
+      />
+      {/*
+ // @ts-ignore */}
+      <HelperText type={"error"} visible={errors ? true : false}>
+        {errors}
+      </HelperText>
+    </>
+  );
+}
