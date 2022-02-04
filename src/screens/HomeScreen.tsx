3f03f19ae28d5fdd6b08c54ef7df5d2e3f0c2f9d
nafees nazik <nafeesnazik10@gmail.com>
2022-02-04 08:54:45 +0530

    update homescreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,8 @@
 import styled, { css } from "@emotion/native";
 import { Header } from "../components/home";
 import { ProjectFabButton } from "../components/home";
+import { useStore } from "../store";
+import { store as Store } from "../selectors";
 import { RootStackScreenProps } from "../types";
 const Container = styled.View({
   marginHorizontal: 20,
@@ -11,12 +13,15 @@ export function HomeScreen({
   route,
   navigation,
 }: RootStackScreenProps<"Home">) {
+  const store = useStore(Store);
+  console.log(store);
+
   return (
     <>
       <Container>
         <Header navigation={navigation} />
       </Container>
-      <ProjectFabButton />
+      <ProjectFabButton navigation={navigation} />
     </>
   );
 }
