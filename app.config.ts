c67384ccca135b3500a2b542567979a04e6c9381
nafees nazik <nafeesnazik10@gmail.com>
2022-02-09 00:08:17 +0530

    bump version

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@
 import { ExpoConfig, ConfigContext } from "@expo/config";
 
 export const APP_NAME = "Eisen Matrix";
-export const VERSION = "1.0.0";
-export const VERSION_CODE = 1;
+export const VERSION = "1.1.0";
+export const VERSION_CODE = 2;
 export const LICENSE = "MIT";
 export const PACKAGE_NAME = "com.g3root.einsenMatrix";
 export const REPO_Link = "https://github.com/G3root/Einsen-Matrix";
