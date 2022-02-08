bf41f7c4c428ebed5c0474d5aa8f837f52b956f2
nafees nazik <nafeesnazik10@gmail.com>
2022-02-08 22:46:27 +0530

    add filter tabs

--- src/components/project/ProjectList/ProjectList.tsx
+++ src/components/project/ProjectList/ProjectList.tsx
@@ -4,6 +4,8 @@ import { ProjectCard } from "../ProjectCard/ProjectCard";
 import { FlatList, ListRenderItem } from "react-native";
 import { Project } from "../../../types";
 import shallow from "zustand/shallow";
+import { useTheme } from "react-native-paper";
+import { Tabs, TabScreen } from "react-native-paper-tabs";
 
 import styled from "@emotion/native";
 import { EmptyList } from "../../common";
@@ -12,36 +14,103 @@ const Spacer = styled.View({
   marginBottom: 10,
 });
 
+const Container = styled.View({
+  marginHorizontal: 20,
+});
+
 export interface IProjectListProps {}
 
+type ProjectWithKey = Project & { projectKey: string };
+
 export function ProjectList(props: IProjectListProps) {
+  const { colors } = useTheme();
+
   const { keys, store } = useStore(
     (state) => ({ keys: Object.keys(state.data), store: state.data }),
     shallow
   );
 
-  const refinedData = keys.map((key) => ({
-    projectKey: key,
-    ...store[key],
-  }));
+  const allProjects: ProjectWithKey[] = [];
+  const todoProjects: ProjectWithKey[] = [];
+  const doneProjects: ProjectWithKey[] = [];
+
+  for (let index = 0; index < keys.length; index++) {
+    const key = keys[index];
+
+    const project = {
+      projectKey: key,
+      ...store[key],
+    };
+
+    allProjects.push(project);
+
+    if (project.isCompleted) {
+      doneProjects.push(project);
+    } else {
+      todoProjects.push(project);
+    }
+  }
 
   const renderItem: ListRenderItem<Project & { projectKey: string }> = ({
     item,
   }) => <ProjectCard projectKey={item.projectKey} />;
 
   return (
-    <FlatList
-      data={refinedData}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.projectKey}
-      ItemSeparatorComponent={Spacer}
-      ListEmptyComponent={
-        <EmptyList
-          title="No Projects Yet"
-          tagLine="Be sure to add your first Project!"
-        />
-      }
-      contentContainerStyle={{ paddingTop: 50, paddingBottom: 120 }}
-    />
+    <Tabs
+      style={{ backgroundColor: colors.background, marginTop: 20 }}
+      uppercase={false}
+    >
+      <TabScreen label="All" icon="view-list">
+        <Container>
+          <FlatList
+            data={allProjects}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.projectKey}
+            ItemSeparatorComponent={Spacer}
+            ListEmptyComponent={
+              <EmptyList
+                title="No Projects Yet"
+                tagLine="Be sure to add your first Project!"
+              />
+            }
+            contentContainerStyle={{ paddingTop: 50, paddingBottom: 120 }}
+          />
+        </Container>
+      </TabScreen>
+      <TabScreen label="Todo" icon="target">
+        <Container>
+          <FlatList
+            data={todoProjects}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.projectKey}
+            ItemSeparatorComponent={Spacer}
+            ListEmptyComponent={
+              <EmptyList
+                title="No Projects Yet"
+                tagLine="Be sure to add your first Project!"
+              />
+            }
+            contentContainerStyle={{ paddingTop: 50, paddingBottom: 120 }}
+          />
+        </Container>
+      </TabScreen>
+      <TabScreen label="Done" icon="check-all">
+        <Container>
+          <FlatList
+            data={doneProjects}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.projectKey}
+            ItemSeparatorComponent={Spacer}
+            ListEmptyComponent={
+              <EmptyList
+                title="No Projects Yet"
+                tagLine="Be sure to add your first Project!"
+              />
+            }
+            contentContainerStyle={{ paddingTop: 50, paddingBottom: 120 }}
+          />
+        </Container>
+      </TabScreen>
+    </Tabs>
   );
 }
--- src/components/task/TaskList/TaskList.tsx
+++ src/components/task/TaskList/TaskList.tsx
@@ -6,7 +6,8 @@ import styled from "@emotion/native";
 import { EmptyList } from "../../common";
 import { FlatList, ListRenderItem } from "react-native";
 import { TaskItem } from "../../../types";
-import { RootStackScreenProps } from "../../../types";
+import { Tabs, TabScreen } from "react-native-paper-tabs";
+import { useTheme } from "react-native-paper";
 export interface ITaskListProps {
   projectKey: string;
   matrixKey: 1 | 2 | 3 | 4;
@@ -16,37 +17,103 @@ const Spacer = styled.View({
   marginBottom: 10,
 });
 
+const Container = styled.View({
+  marginHorizontal: 20,
+});
+
+type TaskWithId = TaskItem & { id: string };
+
 export function TaskList(props: ITaskListProps) {
   const { matrixKey, projectKey } = props;
-  const data = useStore(
-    (state) => state.data[projectKey].tasks[matrixKey],
+  const { colors } = useTheme();
+  const { data, keys } = useStore(
+    (state) => ({
+      data: state.data[projectKey].tasks[matrixKey],
+      keys: Object.keys(state.data[projectKey].tasks[matrixKey]),
+    }),
     shallow
   );
-  const keys = useStore(
-    (state) => Object.keys(state.data[projectKey].tasks[matrixKey]),
-    shallow
-  );
-  const refinedData = keys.map((key) => ({
-    id: key,
-    ...data[key],
-  }));
+
+  const allTasks: TaskWithId[] = [];
+  const todoTasks: TaskWithId[] = [];
+  const doneTasks: TaskWithId[] = [];
+
+  for (let index = 0; index < keys.length; index++) {
+    const id = keys[index];
+    const task = {
+      id,
+      ...data[id],
+    };
+
+    allTasks.push(task);
+
+    if (task.isCompleted) {
+      doneTasks.push(task);
+    } else {
+      todoTasks.push(task);
+    }
+  }
 
   const renderItem: ListRenderItem<TaskItem & { id: string }> = ({ item }) => (
     <TaskCard taskKey={item.id} projectKey={projectKey} matrixKey={matrixKey} />
   );
 
   return (
-    <FlatList
-      data={refinedData}
-      renderItem={renderItem}
-      keyExtractor={(item) => item.id}
-      ItemSeparatorComponent={Spacer}
-      ListEmptyComponent={
-        <EmptyList
-          title="No tasks yet"
-          tagLine="Be sure to add your first task!"
-        />
-      }
-    />
+    <Tabs
+      style={{ backgroundColor: colors.background, marginTop: 10 }}
+      uppercase={false}
+    >
+      <TabScreen label="All" icon="view-list">
+        <Container>
+          <FlatList
+            data={allTasks}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={Spacer}
+            ListEmptyComponent={
+              <EmptyList
+                title="No tasks yet"
+                tagLine="Be sure to add your first task!"
+              />
+            }
+            contentContainerStyle={{ paddingTop: 50, paddingBottom: 120 }}
+          />
+        </Container>
+      </TabScreen>
+      <TabScreen label="Todo" icon="target">
+        <Container>
+          <FlatList
+            data={todoTasks}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={Spacer}
+            ListEmptyComponent={
+              <EmptyList
+                title="No tasks yet"
+                tagLine="Be sure to add your first task!"
+              />
+            }
+            contentContainerStyle={{ paddingTop: 50, paddingBottom: 120 }}
+          />
+        </Container>
+      </TabScreen>
+      <TabScreen label="Done" icon="check-all">
+        <Container>
+          <FlatList
+            data={doneTasks}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            ItemSeparatorComponent={Spacer}
+            ListEmptyComponent={
+              <EmptyList
+                title="No tasks yet"
+                tagLine="Be sure to add your first task!"
+              />
+            }
+            contentContainerStyle={{ paddingTop: 50, paddingBottom: 120 }}
+          />
+        </Container>
+      </TabScreen>
+    </Tabs>
   );
 }
